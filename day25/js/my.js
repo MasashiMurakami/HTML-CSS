2776e447e4c7ee966b3e52fead151605b2f03561
mm2411 <webmasa@outlook.com>
2021-03-10 20:33:07 +0900

    code crista02 header, concept, works in 3.5hrs.

---
+++ day25/js/my.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.header').vegas({
+    timer: false,
+    cover: true,
+    slides: [
+        { src: '/img/fv-bgi_01@2x.jpg' },
+        { src: '/img/fv-bgi_02@2x.jpg' },
+        { src: '/img/fv-bgi_03@2x.jpg' },
+    ]
+  });
+});
